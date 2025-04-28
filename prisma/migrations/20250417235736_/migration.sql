887e9ed99c24c4ea626a477c11e6b4bac9669b01
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-04-27 21:47:52 -0300

    feat: usando o sqllite

--- prisma/migrations/20250417235736_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "clientes" (
-    "id" SERIAL NOT NULL,
-    "nome" TEXT NOT NULL,
-    "telefone" TEXT NOT NULL,
-
-    CONSTRAINT "clientes_pkey" PRIMARY KEY ("id")
-);
