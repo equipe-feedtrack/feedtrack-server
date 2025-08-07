e4eb46f66a05eeab783ececaa802365bcda62d08
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-08-07 02:39:18 -0300

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,46 +1,41 @@
-import dotenv from 'dotenv';
-import { createHTTPServer } from './presentation/http/server';
-
-
+import dotenv from "dotenv";
+import { createHTTPServer } from "./presentation/http/server";
 
 async function bootstrap() {
+  //Carrega variáveis de ambiente do arquivo .env
+  dotenv.config();
 
-    //Carrega variáveis de ambiente do arquivo .env
-	dotenv.config();
+  const api_name = process.env.API_NAME || "FeedTrack API";
+  const host_name = process.env.HOST_NAME || "localhost";
+  const port = Number(process.env.PORT) || 3006;
 
-    const api_name = process.env.API_NAME || 'FeedTrack API';
-    const host_name = process.env.HOST_NAME || 'localhost';
-    const port = Number(process.env.PORT) || 3006;
+  console.log(`[${api_name}] 🚀 Inicializando a API....`);
 
-    console.log(`[${api_name}] 🚀 Inicializando a API....`);
+  const httpServer = await createHTTPServer();
 
-    const httpServer = await createHTTPServer();
-
-    httpServer.listen({ port: port }, async () => {
-        console.log(`[${api_name}] ✅ Servidor HTTP pronto e ouvindo em http://${host_name}:${port}/api/v1 \n[Swagger Docs] ✅ Swagger http://localhost:3000/api-docs`);
-    });
-
+  httpServer.listen({ port: port }, async () => {
+    console.log(
+      `[${api_name}] ✅ Servidor HTTP pronto e ouvindo em http://${host_name}:${port}/api/v1`
+    );
+    console.log(
+      `[Swagger Docs] ✅ Swagger disponível em http://${host_name}:${port}/api-docs`
+    );
+  });
 }
 
-bootstrap()
-    .catch((error) => {
-        console.error(error);
-    });
+bootstrap().catch((error) => {
+  console.error(error);
+});
 
-
-
-
-
-    
 // ENVIANDO POR EMAIL O FORMULARIO.
 // async function main() {
-  
+
 //     const pessoa = new Pessoa({
 //   nome: "João",
 //   email: "joao@email.com",
 //   telefone: "12345678"
 // });
-  
+
 //     // 1. Criar o cliente
 //   const cliente = Cliente.criarCliente({
 //     pessoa,
@@ -148,8 +143,6 @@ bootstrap()
 
 // console.log(feedback)
 
-
-
 // Cleilson.
 
 // import express from 'express';
@@ -191,15 +184,13 @@ bootstrap()
 //           telefone: true
 //         }
 //       });
-  
+
 //       res.json(dados);
 //     } catch (error) {
 //       console.error('Erro ao buscar dados:', error);
 //       res.status(500).json({ erro: 'Erro ao buscar dados' });
 //     }
 //   });
-  
-
 
 // app.listen(3000, () => {
 //   console.log('Server is running on http://localhost:3000');
